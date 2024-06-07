9da50fa24a13ce1580c733b9efbb323430c79f95
Maximilian Bundscherer <maximilian@bundscherer-online.de>
2024-06-07 09:37:57 +0200

    Init

--- backend/TestService.py
+++ backend/TestService.py
@@ -1,3 +1,4 @@
+import time
 from dataclasses import dataclass
 
 
@@ -6,16 +7,19 @@ class TestService:
     class TestItem:
         name: str
         description: str
+        created_at: str = ''
 
     @staticmethod
     def get_test_items() -> [TestItem]:
         return [
             TestService.TestItem(
                 name='Test 1',
-                description='This is a test item.'
+                description='This is a test item.',
+                created_at=time.strftime('%Y-%m-%d %H:%M:%S')
             ),
             TestService.TestItem(
                 name='Test 2',
-                description='This is another test item.'
+                description='This is another test item.',
+                created_at=time.strftime('%Y-%m-%d %H:%M:%S')
             )
         ]
