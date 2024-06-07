4abf8baee4a750b97602d2937d095be0f49a979c
Maximilian Bundscherer <maximilian@bundscherer-online.de>
2024-06-07 11:40:15 +0200

    Init

--- backend/antRotator/AbstractAntRotatorService.py
+++ backend/antRotator/AbstractAntRotatorService.py
@@ -1,7 +1,21 @@
+from dataclasses import dataclass
+
+
 class AbstractAntRotatorService:
+    @dataclass
+    class AntRotatorState:
+        azimuth: float
+        elevation: float
 
     def __init__(self):
+        self._state: AbstractAntRotatorService.AntRotatorState = AbstractAntRotatorService.AntRotatorState(
+            azimuth=-1,
+            elevation=-1
+        )
         print("- AntRotatorService initialized.")
 
     def test(self):
+        raise NotImplementedError("Method must be implemented in derived classes.")
+
+    def get_state(self) -> AntRotatorState:
         raise NotImplementedError("Method must be implemented in derived classes.")
--- backend/radioControl/AbstractRadioControlService.py
+++ backend/radioControl/AbstractRadioControlService.py
@@ -1,7 +1,23 @@
+from dataclasses import dataclass
+
+
 class AbstractRadioControlService:
+    @dataclass
+    class RadioState:
+        frequency: float
+        mode: str
+        is_transmitting: bool
 
     def __init__(self):
+        self._state: AbstractRadioControlService.RadioState = AbstractRadioControlService.RadioState(
+            frequency=-1,
+            mode='N/A',
+            is_transmitting=False
+        )
         print("- RadioControlService initialized.")
 
     def test(self):
+        raise NotImplementedError("Method must be implemented in derived classes.")
+
+    def get_state(self) -> RadioState:
         raise NotImplementedError("Method must be implemented in derived classes.")
