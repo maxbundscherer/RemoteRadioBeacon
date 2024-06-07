a7570ccc964e253eb23333c671ebd520b448d96e
Maximilian Bundscherer <maximilian@bundscherer-online.de>
2024-06-07 12:23:21 +0200

    Init

--- backend/radioControl/DummyRadioControlService.py
+++ backend/radioControl/DummyRadioControlService.py
@@ -12,8 +12,8 @@ class DummyRadioControlService(AbstractRadioControlService):
 
     def _trigger_update_state(self):
         self._state = RadioState(
-            frequency=random.uniform(0, 1500),
-            mode=['LSB', 'USB', 'CW', 'AM', 'FM', 'N/A'][random.randint(0, 5)],
+            frequency=int(random.uniform(430e6, 440e6)),
+            mode=['LSB', 'USB', 'CW', 'AM', 'FM'][random.randint(0, 4)],
             is_transmitting=random.choice([True, False]),
             last_updated=TimeUtil.get_current_time_utc_str()
         )
