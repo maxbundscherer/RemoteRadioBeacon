dd0571740c2d971cd12f5b131ec0462f9396d25d
Maximilian Bundscherer <maximilian@bundscherer-online.de>
2024-06-07 10:19:15 +0200

    Init

--- app.py
+++ app.py
@@ -11,18 +11,25 @@ app = Flask(
 
 @app.route('/')
 def route_index():
-    page_html = "<p>Please make a selection:</p>"
+    page_html = "<p>Please make a selection:</p><ul>"
     for url_title, url_target in [
-        # ('Home', url_for('route_index')),
-        ('Test', url_for('route_test')),
-        # ('API Test', url_for('route_api_test')),
         ('Control', url_for('route_control')),
+        ('Test', url_for('route_test')),
         ('About', url_for('route_about')),
     ]:
-        page_html += f"<p><a href='{url_target}'>{url_title}</a></p>"
+        page_html += f'<li><a href="{url_target}">{url_title}</a></li>'
+    page_html += "</ul>"
     return render_template('simple_html.html',
                            site_title='Home',
                            page_html=page_html,
+                           )
+
+
+@app.route('/control')
+def route_control():
+    return render_template('simple_content.html',
+                           site_title='Control',
+                           page_content='Hello World!',
                            )
 
 
@@ -41,14 +48,6 @@ def route_test():
 
     return render_template('test.html',
                            site_title='Test',
-                           )
-
-
-@app.route('/control')
-def route_control():
-    return render_template('simple_content.html',
-                           site_title='Control',
-                           page_content='Hello World!',
                            )
 
 
