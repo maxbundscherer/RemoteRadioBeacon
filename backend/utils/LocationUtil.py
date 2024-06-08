c27240f2a8187c56ae5b1c80802145627c2b7b16
Maximilian Bundscherer <maximilian@bundscherer-online.de>
2024-06-08 12:33:56 +0200

    Init

--- backend/utils/LocationUtil.py
+++ backend/utils/LocationUtil.py
@@ -1,5 +1,6 @@
 from dataclasses import dataclass
 import maidenhead as mh
+from pyproj import Geod
 
 
 class LocationUtil:
@@ -7,6 +8,11 @@ class LocationUtil:
     class Coordinate:
         latitude: float
         longitude: float
+
+    @dataclass
+    class DistanceAzimuth:
+        distance: float
+        azimuth: float
 
     @staticmethod
     def maidenhead_to_coordinates(loc: str) -> Coordinate:
@@ -17,10 +23,28 @@ class LocationUtil:
             longitude=c[1]
         )
 
+    @staticmethod
+    def calc_distance_azimuth(loc_0: Coordinate, loc_1: Coordinate) -> DistanceAzimuth:
+        eod = Geod(ellps="WGS84")
+
+        azimuth0, azimuth1, distance = eod.inv(loc_0.longitude, loc_0.latitude, loc_1.longitude, loc_1.latitude)
+
+        azimuth0 = (azimuth0 + 360) % 360
+        distance = distance / 1000
+
+        return LocationUtil.DistanceAzimuth(
+            distance=distance,
+            azimuth=azimuth0
+        )
+
 
 if __name__ == '__main__':
     gps_0: LocationUtil.Coordinate = LocationUtil.maidenhead_to_coordinates("JN59NK18")
-    gps_1: LocationUtil.Coordinate = LocationUtil.maidenhead_to_coordinates("JN58MP36")
+    gps_1: LocationUtil.Coordinate = LocationUtil.maidenhead_to_coordinates("JN58SD15")
 
     print(gps_0)
     print(gps_1)
+
+    da = LocationUtil.calc_distance_azimuth(gps_0, gps_1)
+
+    print(da)
