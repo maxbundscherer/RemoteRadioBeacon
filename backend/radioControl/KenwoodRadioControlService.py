d1d4a15b037c05765d06dd0a71626edcd70b3cc6
Maximilian Bundscherer <maximilian@bundscherer-online.de>
2024-06-18 10:15:13 +0200

    refactor

--- backend/radioControl/KenwoodRadioControlService.py
+++ backend/radioControl/KenwoodRadioControlService.py
@@ -3,15 +3,17 @@ import os
 import random
 from concurrent.futures import ThreadPoolExecutor
 
+from backend.ConfigService import ConfigService
 from backend.radioControl.AbstractRadioControlService import AbstractRadioControlService, RadioState
 from backend.utils.TimeUtil import TimeUtil
 
 
 class KenwoodRadioControlService(AbstractRadioControlService):
 
-    def __init__(self, config_service):
+    def __init__(self, config_service: ConfigService):
         self._is_transmitting: bool = False
         self._executor = ThreadPoolExecutor()
+        self._config_service: ConfigService = config_service
         super().__init__(config_service)
 
     @staticmethod
@@ -20,14 +22,74 @@ class KenwoodRadioControlService(AbstractRadioControlService):
         return os.system("rigctl -V") == 0
 
     def get_state(self) -> RadioState:
-        # TODO
-        return RadioState(
-            frequency=-1,
-            power=-1,
-            mode="error",
-            is_transmitting=self._is_transmitting,
-            last_updated=TimeUtil.get_current_time_utc_str()
-        )
+
+        if self._is_transmitting:
+            print("WARNING: KenwoodRadioControlService.get_state() called while transmitting. Ignoring.")
+            return RadioState(
+                frequency=-1,
+                power=-1,
+                mode="in_transmit",
+                is_transmitting=self._is_transmitting,
+                last_updated=TimeUtil.get_current_time_utc_str()
+            )
+
+        error_flag = False
+
+        # print("Get Freq")
+        r_freq: int = 0
+        output = (os.popen(
+            f'rigctl -m 2014 -s 38400 -r {self._config_service.get_config().radio_control_device} -P {self._config_service.get_config().radio_control_device} --set-conf=rts_state="OFF",dtr_state="OFF" get_freq')
+                  .read()
+                  ).split("\n")
+        # print(f"Output [{len(output)}]  '{output}'")
+        if len(output) == 3:
+            r_freq: int = int(output[1])
+            # print(f"Freq  '{r_freq}'")
+        else:
+            error_flag = True
+
+        # print("Get Mode")
+        r_mode: str = ""
+        output = (os.popen(
+            f'rigctl -m 2014 -s 38400 -r {self._config_service.get_config().radio_control_device} -P {self._config_service.get_config().radio_control_device} --set-conf=rts_state="OFF",dtr_state="OFF" get_mode')
+                  .read()
+                  ).split("\n")
+        # print(f"Output [{len(output)}]  '{output}'")
+        if len(output) == 4:
+            r_mode: str = str(output[1])
+            # print(f"Mode  '{r_mode}'")
+        else:
+            error_flag = True
+
+        # print("Get Power")
+        r_power: int = 0
+        output = (os.popen(
+            f'rigctl -m 2014 -s 38400 -r {self._config_service.get_config().radio_control_device} -P {self._config_service.get_config().radio_control_device} --set-conf=rts_state="OFF",dtr_state="OFF" get_level RFPOWER')
+                  .read()
+                  ).split("\n")
+        # print(f"Output [{len(output)}]  '{output}'")
+        if len(output) == 3:
+            r_power: int = int(float(output[1]) * 100)
+            # print(f"Power  '{r_power}'")
+        else:
+            error_flag = True
+
+        if error_flag:
+            return RadioState(
+                frequency=-1,
+                power=-1,
+                mode="error",
+                is_transmitting=self._is_transmitting,
+                last_updated=TimeUtil.get_current_time_utc_str()
+            )
+        else:
+            return RadioState(
+                frequency=r_freq,
+                power=r_power,
+                mode=r_mode,
+                is_transmitting=self._is_transmitting,
+                last_updated=TimeUtil.get_current_time_utc_str()
+            )
 
     # async def _impl_async_update(self):
     #
